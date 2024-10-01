ed3ce701dcf07552bdd05714183b687ede18d3a4
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-10-01 13:18:49 +0100

    Creating the schema for the users

---
+++ Db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, varchar, text, timestamp, boolean, integer } from 'drizzle-orm/pg-core';
+
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
